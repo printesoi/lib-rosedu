0ab9b7be897dff39d5c0d447e40016fba26c693b
Victor Dodon <dodonvictor@gmail.com>
2012-09-10 15:09:49 +0300

    Model User -> Author

--- db/migrate/20120910120504_create_authors.rb
+++ db/migrate/20120910120504_create_authors.rb
@@ -1,6 +1,6 @@
-class CreateUsers < ActiveRecord::Migration
+class CreateAuthors < ActiveRecord::Migration
   def self.up
-    create_table :users do |t|
+    create_table :authors do |t|
       t.string :first_name
       t.string :last_name
       t.string :middle_name
@@ -10,6 +10,6 @@ class CreateUsers < ActiveRecord::Migration
   end
 
   def self.down
-    drop_table :users
+    drop_table :authors
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120910114029) do
+ActiveRecord::Schema.define(:version => 20120910113111) do
 
   create_table "authors", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "middle_name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", :force => true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "middle_name"
