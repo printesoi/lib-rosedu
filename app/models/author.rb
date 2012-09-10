98ba7ffe93c656095f07b7042b08a0367e2af342
Victor Dodon <dodonvictor@gmail.com>
2012-09-10 15:38:17 +0300

    Valideaza prezenta numelui si prenumelui. Adauga teste pentru asta

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ActiveRecord::Base
+    attr_accessible :first_name, :last_name, :middle_name
+
+    validates :first_name, :presence => true
+    validates :last_name, :presence => true
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe Author do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+    before(:each) do
+        @attrs = { :first_name => "First", :last_name => "Last", :middle_name => "Middle" }
+    end
+
+    it "should create a new instance given a valid attribute" do
+        Author.create!(:first_name => "First", :last_name => "Last", :middle_name => "Middle")
+    end
+
+    it "should require first name" do
+        no_name_author = Author.new(@attrs.merge(:first_name => ""))
+        no_name_author.should_not be_valid
+    end
+
+    it "should require last name" do
+        no_name_author = Author.new(@attrs.merge(:last_name => ""))
+        no_name_author.should_not be_valid
+    end
 end
