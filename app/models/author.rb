ab169aab29e6f6056409c1c700569ee86e9082e5
Victor Dodon <dodonvictor@gmail.com>
2012-09-24 23:19:43 +0300

    Polish in author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,12 +1,12 @@
 class Author < ActiveRecord::Base
     attr_accessible :first_name, :last_name, :middle_name
 
-    has_and_belongs_to_many :book
+    has_and_belongs_to_many :books
 
     validates :first_name, :presence => true
- #   validates :last_name, :presence => true
+    validates :last_name, :presence => true
 
     def name
-    	first_name + ' ' + last_name
+        first_name + ' ' + last_name
     end
 end
