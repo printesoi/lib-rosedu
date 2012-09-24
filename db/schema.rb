c32fa4cef09da2aabdd32cd65ad4ba31e24d5c6c
Victor Dodon <dodonvictor@gmail.com>
2012-09-24 23:35:53 +0300

    Make some checks in User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120924195857) do
+ActiveRecord::Schema.define(:version => 20120924202435) do
 
   create_table "authors", :force => true do |t|
     t.string   "first_name"
@@ -40,6 +40,16 @@ ActiveRecord::Schema.define(:version => 20120924195857) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "phone"
+    t.string   "extra"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
