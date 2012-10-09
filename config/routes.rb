34a5931c1332bfff6a3cfe1c6b0bdf1f337ec737
Victor Dodon <dodonvictor@gmail.com>
2012-10-09 20:05:48 +0300

    Make books resources

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ LibRosedu::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
+  resources :books
+
   match 'books' => 'book#index'
   match 'add' => 'book#add'
 
@@ -51,7 +53,7 @@ LibRosedu::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "book#index"
 
   # See how all your routes lay out with "rake routes"
 
