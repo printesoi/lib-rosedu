62c018ae7afba94b846857924b54d509dc9c883d
Victor Dodon <dodonvictor@gmail.com>
2012-10-09 20:05:13 +0300

    Change the full_method in Author class

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,7 +6,11 @@ class Author < ActiveRecord::Base
     validates :first_name, :presence => true
     validates :last_name, :presence => true
 
-    def name
-        first_name + ' ' + last_name
+    def full_name
+        if middle_name.blank?
+            "#{last_name}, #{first_name}"
+        else
+            "#{last_name}, #{first_name} #{middle_name}"
+        end
     end
 end
