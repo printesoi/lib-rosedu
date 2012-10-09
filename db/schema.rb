cffcc13107da3a584cfe8ee37f02d7636340da6c
Victor Dodon <dodonvictor@gmail.com>
2012-10-09 20:30:54 +0300

    Add index on the email in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121009172253) do
+ActiveRecord::Schema.define(:version => 20121009172849) do
 
   create_table "admins", :force => true do |t|
     t.string   "name"
@@ -79,5 +79,7 @@ ActiveRecord::Schema.define(:version => 20121009172253) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
 
 end
